8874cb704bd3a4a8ecda9ae37288ea125ca9ff1c
Jian Xu <jianxu1@microsoft.com>
2024-03-30 16:25:59 +0800

    update

--- Program.cs
+++ Program.cs
@@ -1,15 +1,13 @@
 ﻿using log4net;
 using System;
-using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Automation;
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
 public class SITAO
 {
-    private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-    private static string windowName = null; // Initialize to null to indicate it's optional
+    private static readonly ILog log = LogManager.GetLogger(typeof(SITAO));
+    private static string windowName = null;
     private static string hyperlinkText;
 
     public static void Main(string[] args)
@@ -21,16 +19,14 @@ public class SITAO
         }
 
         hyperlinkText = args[0];
-        if (args.Length > 1) // Window name is provided as an optional second argument
+        if (args.Length > 1)
         {
             windowName = args[1];
         }
 
         log.Info($"Starting monitoring for hyperlink: {hyperlinkText}" + (windowName != null ? $" in window: {windowName}" : "") + "...");
 
-        // Start a separate thread for monitoring
-        Thread monitorThread = new Thread(MonitorWindows);
-        monitorThread.IsBackground = true;
+        Thread monitorThread = new Thread(MonitorWindows) { IsBackground = true };
         monitorThread.Start();
 
         Console.WriteLine("Press any key to exit...");
@@ -42,13 +38,12 @@ public class SITAO
         while (true)
         {
             TryFindAndInvokeHyperlinks();
-            Thread.Sleep(1000); // Wait for 1 second before trying again
+            Thread.Sleep(500);
         }
     }
 
     private static void TryFindAndInvokeHyperlinks()
     {
-        // Retrieve the root element on the desktop
         AutomationElement rootElement = AutomationElement.RootElement;
         var hyperLinkCondition = new AndCondition(
             new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Hyperlink),
@@ -57,7 +52,6 @@ public class SITAO
 
         if (windowName != null)
         {
-            // If a window name is specified, search for the window
             var windowCondition = new PropertyCondition(AutomationElement.NameProperty, windowName);
             AutomationElement window = rootElement.FindFirst(TreeScope.Children, windowCondition);
             if (window != null)
@@ -71,19 +65,18 @@ public class SITAO
         }
         else
         {
-            // No window name specified, search through all top-level windows
             var windows = rootElement.FindAll(TreeScope.Children, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Window));
-            Parallel.ForEach(windows.Cast<AutomationElement>(), window =>
+            foreach (AutomationElement window in windows)
             {
                 FindAndInvokeHyperlinksInWindow(window, hyperLinkCondition);
-            });
+            }
         }
     }
 
     private static void FindAndInvokeHyperlinksInWindow(AutomationElement window, Condition hyperLinkCondition)
     {
         var hyperLinks = window.FindAll(TreeScope.Descendants, hyperLinkCondition);
-        Parallel.ForEach(hyperLinks.Cast<AutomationElement>(), hyperLink =>
+        foreach (AutomationElement hyperLink in hyperLinks)
         {
             if (hyperLink.TryGetCurrentPattern(InvokePattern.Pattern, out object pattern))
             {
@@ -95,6 +88,6 @@ public class SITAO
             {
                 log.Warn($"Hyperlink: {hyperlinkText} in window: {window.Current.Name} could not be invoked.");
             }
-        });
+        }
     }
 }
